6d931a2152fe71c258e3544c22f43d709f4ef117
PaluiBanar <akbridha@gmail.com>
2023-07-08 13:38:07 +0700

    satu pertanyaan selesai, include animasi, handle jawaban salah, jawaban benar, jawaban sudah benar semua, dan menampilkan Gif

--- app/src/main/java/com/example/rotah/AnimActivity.kt
+++ app/src/main/java/com/example/rotah/AnimActivity.kt
@@ -1,41 +1,110 @@
 package com.example.rotah
 
 import android.animation.ObjectAnimator
+import android.media.MediaPlayer
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.animation.Animation
 import android.view.animation.TranslateAnimation
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.bumptech.glide.Glide
 import com.example.rotah.databinding.ActivityAnimBinding
 import com.example.rotah.databinding.ActivityLevelSatuBinding
 
 class AnimActivity : AppCompatActivity() {
     private lateinit var binding :  ActivityAnimBinding
-    private var isSatuMoved = false
+    private val booleanJawabanStatus = booleanArrayOf(false, false, false, false)
+    private var mediaPlayer: MediaPlayer? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityAnimBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        supportActionBar?.hide()
+        mediaPlayer = MediaPlayer.create(this, R.raw.congrat)
 
 
-        // Atur listener klik pada button
+        binding.imageViewGif.visibility = View.INVISIBLE
+        binding.imageViewCenter.visibility = View.INVISIBLE
+
+        Toast.makeText(this@AnimActivity, "Apa Bahasa Arab benda yang dipegang anak tersebut..? ", Toast.LENGTH_LONG).show()
+
+        binding.harfuAlif.setOnClickListener {
+            // Ambil posisi X dan Y saat ini dari ImageView
+            val startX = binding.harfuAlif.left
+            val startY = binding.harfuAlif.top
+            // Ambil posisi X dan Y dari Button
+            val endX = binding.dua.left
+            val endY = binding.dua.top
+            Animate(binding.harfuAlif, startX, startY, endX, endY)
+            booleanJawabanStatus[0] = true
+            cekStatusSemuaJawaban()
+        }
+        binding.harfuTa.setOnClickListener {
+
+            // Ambil posisi X dan Y saat ini dari ImageView
+            val startX = binding.harfuTa.left
+            val startY = binding.harfuTa.top
+            // Ambil posisi X dan Y dari Button
+            val endX = binding.tiga.left
+            val endY = binding.tiga.top
+            Animate(binding.harfuTa, startX, startY, endX, endY)
+
+            booleanJawabanStatus[1] = true
+            cekStatusSemuaJawaban()
+        }
+        binding.harfuTho.setOnClickListener {
+
+              Toast.makeText(this@AnimActivity, "Salah.. clue : pilihlah huruf untuk 'kitab'", Toast.LENGTH_SHORT).show()
+        }
+        binding.harfuKaf.setOnClickListener {
+            // Ambil posisi X dan Y saat ini dari ImageView
+            val startX = binding.harfuKaf.left
+            val startY = binding.harfuKaf.top
+            // Ambil posisi X dan Y dari Button
+            val endX = binding.empat.left
+            val endY = binding.empat.top
+            Animate(binding.harfuKaf, startX, startY, endX, endY)
+              booleanJawabanStatus[2] = true
+              cekStatusSemuaJawaban()
+        }
         binding.harfuBa.setOnClickListener {
             // Ambil posisi X dan Y saat ini dari ImageView
-
             val startX = binding.harfuBa.left
             val startY = binding.harfuBa.top
-
             // Ambil posisi X dan Y dari Button
             val endX = binding.satu.left
             val endY = binding.satu.top
             Animate(binding.harfuBa, startX, startY, endX, endY)
+              booleanJawabanStatus[3] = true
+              cekStatusSemuaJawaban()
+        }
+        binding.harfuQaf.setOnClickListener {
 
 
+
+              Toast.makeText(this@AnimActivity, "Salah.. ", Toast.LENGTH_SHORT).show()
+              Toast.makeText(this@AnimActivity, "clue : pilihlah huruf untuk 'kitab'", Toast.LENGTH_SHORT).show()
         }
 
+    }
+
+    private fun cekStatusSemuaJawaban() {
+        for (i in booleanJawabanStatus.indices) {
+            Log.d("TAG", "booleanArray[$i]: ${booleanJawabanStatus[i]}")
+        }
+        val allTrue = booleanJawabanStatus.all { it }
+        if (allTrue){
+
+            mediaPlayer?.start()
+            Toast.makeText(this@AnimActivity, "Selamat  jawaban Benar ", Toast.LENGTH_SHORT).show()
+            binding.imageViewGif.visibility = View.VISIBLE
+            binding.imageViewCenter.visibility = View.VISIBLE
+        }
     }
 
     private fun Animate(view: View, startX: Int, startY: Int, endX: Int, endY: Int) {
--- app/src/main/java/com/example/rotah/MainActivity.kt
+++ app/src/main/java/com/example/rotah/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.rotah
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
+import android.view.animation.TranslateAnimation
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -13,15 +17,40 @@ import com.example.rotah.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val SPLASH_DELAY: Long = 3000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        supportActionBar?.hide()
 
-        binding.btnStart.setOnClickListener{
-            startActivity(Intent(this@MainActivity, LevelSatu::class.java))
-        }
+
+
+
+
+        Handler().postDelayed({
+            val fadeInAnimation = AlphaAnimation(0f, 1f) // Membuat efek fade-in dari transparansi 0 hingga 1
+            fadeInAnimation.duration = 1000 // Durasi animasi fade-in dalam milidetik (1 detik)
+
+            binding.fadeInTextView.startAnimation(fadeInAnimation)
+
+            // Mengatur animasi dengan TranslateAnimation
+            val animation = TranslateAnimation(0f, 1000f, 0f, 0f) // Menggeser dari kiri ke kanan sejauh 1000 piksel
+            animation.duration = 3000 // Durasi animasi dalam milidetik (3 detik)
+            animation.repeatCount = Animation.INFINITE // Mengulang animasi secara terus-menerus
+            animation.repeatMode = Animation.REVERSE // Membalik arah animasi setelah mencapai titik akhir
+
+            binding.textView.startAnimation(animation)
+
+            // Mengatur penundaan dan memulai Activity baru setelah 3 detik
+            Handler().postDelayed({
+                val intent = Intent(this, AnimActivity::class.java)
+                startActivity(intent)
+                finish() // Menutup Splash Activity agar tidak bisa kembali dengan tombol "Back"
+            }, SPLASH_DELAY)
+        }, 500)
+
     }
 }
